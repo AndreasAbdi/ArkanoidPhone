8d6e04cd7bd9bcd6ce33172aa68de27cc1365136
AndreasAbdi <andreasabdi@yahoo.ca>
2016-11-16 15:09:51 -0800

    Initial project data
    Basic Android project initiation. Version 15 OS.

---
+++ app/src/main/java/abdi/andreas/arkanoid/Arkanoid.java
@@ -0,0 +1,13 @@
+package abdi.andreas.arkanoid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Arkanoid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arkanoid);
+    }
+}
